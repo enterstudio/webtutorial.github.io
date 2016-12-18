b11686ad71eed0cb5fa634b9272cea607fa2f3ec
anjar arizenta <anjar arizenta>
2016-12-18 22:07:30 +0700

    anyar neh,,h

--- tutorial-js/lesson1.js
+++ tutorial-js/lesson1.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    $("#nilai_awal").keyup(function(){
+	    $("#nilai_awal").keyup(function(){
         $("#hasil_nilai").html($("#nilai_awal").val());
+		$("#script_hasil_nilai").html($("#nilai_awal").val());
     }); 
 });
